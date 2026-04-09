0ad7da631b602cb736d3979e4b914032275f8b13
Lonardo <silvaleonardo1899@gmail.com>
2026-04-09 17:11:58 -0300

    Add files via upload

--- htmlsemantico/script.js
+++ htmlsemantico/script.js
@@ -26,6 +26,38 @@ function alterarNome() {
         document.getElementById('nomeStatus').innerHTML = trocarstatus;
     }
 }
+function mudarCorHeader() {
+    let cor = document.getElementById('corHeader').value;
+    document.querySelector('.cabecalho').style.backgroudColor = cor;
+}
+function mudarCorNav() {
+    let cor = document.getElementById('corNav').value;
+    document.querySelector('.navegacao').style.backgroudColor = cor;
+}
+
+function mudarCorSesao() {
+    let cor = document.getElementById('corSesao').value;
+    document.querySelector('.secao').style.backgroudColor = cor;
+}
+function mudarCorFotter() {
+    let cor = document.getElementById('corFotter').value;
+    document.querySelector('.Fotter').style.backgroudColor = cor;
+}
+function mudarCorRodape() {
+    let cor = document.getElementById('corRodape').value;
+    document.querySelector('.rodape').style.backgroudColor = cor;
+}
+
+
+
+
+
+
+
+
+
+
+
 
             
     
